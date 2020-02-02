f17f1116c4437863e53de8f77bd907c740837811
Endre Vegh <endre.vegh@yahoo.com>
2020-02-02 18:41:20 +0100

    connect to postgres db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,39 @@
 import Express from "express";
+import { createConnection } from "typeorm";
 
 const app = Express();
 
-app.listen(4000, () => {
-  console.log("app running");
-});
+const main = async () => {
+  let retryAttempts = 10;
+
+  let connected: boolean = false;
+
+  while (retryAttempts > 0 && !connected) {
+    try {
+      await createConnection({
+        name: "default",
+        type: "postgres",
+        host: "postgres",
+        port: 5432,
+        username: "postgres",
+        password: "postgres_password",
+        database: "postgres",
+        synchronize: true,
+        logging: true,
+        entities: ["src/entities/*.*"]
+      });
+
+      connected = true;
+
+      retryAttempts--;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  app.listen(4000, () => {
+    console.log("app running");
+  });
+};
+
+main();
