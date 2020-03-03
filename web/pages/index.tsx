768c842cd056f079074179919a064ba2fc9d516a
Endre Vegh <endre.vegh@yahoo.com>
2020-03-03 13:39:10 +0100

    add value to verify email

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,10 @@
+import * as React from "react";
+
 import Link from "next/link";
 
 const Home = () => {
+  const [value, handleChange] = React.useState("");
+
   return (
     <>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
@@ -24,10 +28,11 @@ const Home = () => {
       >
         <div style={{ display: "flex", justifyContent: "center" }}>
           home route
+          <input value={value} onChange={e => handleChange(e.target.value)} />
           <button
             onClick={() => {
-              fetch("http://localhost:3050/verify_email/3453", {
-                method: "POST",
+              fetch(`http://localhost:3050/verify_email/${value}`, {
+                method: "GET",
                 credentials: "include"
               });
             }}
