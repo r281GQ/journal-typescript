f914b63ea221ac283ec8b293ad27df4064bc755c
Endre Vegh <endre.vegh@yahoo.com>
2020-03-04 14:21:13 +0100

    mutate payload in auth middleware

--- api/src/middlewares/isAuth.ts
+++ api/src/middlewares/isAuth.ts
@@ -3,23 +3,28 @@ import { MiddlewareFn } from "type-graphql";
 
 import { ACCESS_TOKEN_SECRET } from "../Environment";
 import { ApiContext } from "../types/ApiContext";
+import { Payload } from "../types/Payload";
 
-const getAuthHeader = (context: ApiContext): string => {
+const getAuthHeader = (context: ApiContext): Payload => {
   try {
     const header = context.req.headers["authorization"];
 
     const bearer = header?.split(" ")[1] || "";
 
-    verify(bearer, ACCESS_TOKEN_SECRET);
+    const payload = verify(bearer, ACCESS_TOKEN_SECRET) as Payload;
 
-    return bearer;
-  } catch (e) {
-    throw new Error("not authorized");
+    return payload;
+  } catch {
+    throw new Error("Not authorized!");
   }
 };
 
 export const isAuth: MiddlewareFn<ApiContext> = async ({ context }, next) => {
-  getAuthHeader(context);
+  const { user } = getAuthHeader(context);
+
+  context.payload = {
+    user
+  };
 
   await next();
 };
