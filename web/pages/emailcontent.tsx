7978a9fdca087bd0fe413e154c31375311a61145
Endre Vegh <endre.vegh@yahoo.com>
2020-03-05 16:00:35 +0100

    add layout to emailcontent

--- web/pages/emailcontent.tsx
+++ web/pages/emailcontent.tsx
@@ -1,7 +1,12 @@
 import withAuth from "../utils/withAuth";
+import Layout from "../components/Layout";
 
 const EmailContent = () => {
-  return <div>this page requires the email to be verified</div>;
+  return (
+    <Layout>
+      <div>this page requires the email to be verified</div>
+    </Layout>
+  );
 };
 
 export default withAuth(EmailContent);
