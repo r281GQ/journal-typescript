82c2271f270afe9f786a8c9f3427cb29915213ca
Endre Vegh <endre.vegh@yahoo.com>
2020-03-09 18:52:26 +0100

    createSchema for api

---
+++ api/src/utils/CreateSchema.ts
@@ -0,0 +1,32 @@
+import { buildSchema } from "type-graphql";
+
+import { ChangePassword } from "../resolvers/ChangePassword";
+import { CreateUser } from "../resolvers/CreateUserResolver";
+import { ForgotPassword } from "../resolvers/ForgotPasswordResolver";
+import { LoginResolver } from "../resolvers/LoginResolver";
+import { LogoutResolver } from "../resolvers/LogoutResolver";
+import { Me } from "../resolvers/MeResolver";
+import { ResetPassword } from "../resolvers/ResetPasswordResolver";
+import { SendMail } from "../resolvers/SendEmailResolver";
+import { VerifyEmail } from "../resolvers/VerifyEmailresolver";
+
+export const createSchema = async () => {
+  const mutations = [
+    ChangePassword,
+    CreateUser,
+    ForgotPassword,
+    LoginResolver,
+    LogoutResolver,
+    ResetPassword,
+    SendMail,
+    VerifyEmail
+  ];
+
+  const queries = [Me];
+
+  const schema = await buildSchema({
+    resolvers: [...queries, ...mutations]
+  });
+
+  return schema;
+};
