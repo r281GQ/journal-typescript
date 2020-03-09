025cb627fc6b57bb65227af063b7149c70e3673d
Endre Vegh <endre.vegh@yahoo.com>
2020-03-09 19:27:58 +0100

    modify email to show actual content

--- web/pages/emailcontent.tsx
+++ web/pages/emailcontent.tsx
@@ -1,10 +1,18 @@
-import withAuth from "../utils/withAuth";
 import Layout from "../components/Layout";
+import { useUsersQuery } from "../generated/graphql";
+import withAuth from "../utils/withAuth";
 
 const EmailContent = () => {
+  const { data } = useUsersQuery();
+
   return (
     <Layout>
-      <div>this page requires the email to be verified</div>
+      <div style={{ marginBottom: 24 }}>
+        this page requires the email to be verified
+      </div>
+      {data?.users.map(user => (
+        <div key={user.id}> {`id: ${user.id}, email: ${user.email}`} </div>
+      ))}
     </Layout>
   );
 };
