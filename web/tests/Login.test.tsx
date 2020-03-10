f41edb4fe971d17bff9d2e9481ab4f538906758a
Endre Vegh <endre.vegh@yahoo.com>
2020-03-10 19:24:38 +0100

    expand first test

--- web/tests/Login.test.tsx
+++ web/tests/Login.test.tsx
@@ -1,5 +1,28 @@
-describe("Login", () => {
-  it("pass", () => {});
+import { render } from "@testing-library/react";
+import { MockedProvider } from "@apollo/react-testing";
+
+import { MeDocument } from "../generated/graphql";
+import Login from "./../pages/login";
+
+describe(Login, () => {
+  it("test", () => {
+    const result = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: MeDocument
+            },
+            result: {
+              data: { me: { id: 9, email: "", verified: false } }
+            }
+          }
+        ]}
+      >
+        <Login alreadyLoggedIn={false} />
+      </MockedProvider>
+    );
+  });
 });
 
 export {};
