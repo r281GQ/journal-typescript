a7fa87ca43752aaa37f7062d76d76a244106272a
Endre Vegh <endre.vegh@yahoo.com>
2020-03-22 13:45:46 +0100

    add basic layout

--- web/pages/create_entry.tsx
+++ web/pages/create_entry.tsx
@@ -1,20 +1,69 @@
 const CreateEntry = () => {
   return (
-    <div>
-      <label htmlFor="title">title</label>
-      <input id="title" name="title" />
-      <label htmlFor="body">body</label>
-      <input id="body" name="body" />
-      <label htmlFor="tags">tags</label>
-      <select id="tags">
-        <option>test</option>
-      </select>
-      <div>
-        <button>submit</button>
-      </div>
-      <div>
-        <button>cancel</button>
-      </div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
+        <div
+          style={{
+            margin: "12px 0",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <label style={{ display: "block" }} htmlFor="title">
+            title
+          </label>
+          <input id="title" name="title" />
+        </div>
+        <div
+          style={{
+            margin: "12px 0",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <label htmlFor="body">body</label>
+          <textarea id="body" name="body" />
+        </div>
+        <div
+          style={{
+            margin: "12px 0",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <label htmlFor="tags">tags</label>
+          <select id="tags">
+            <option>test</option>
+          </select>
+        </div>
+        <div
+          style={{
+            margin: "12px 0",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <div style={{ margin: "0 4px" }}>
+            <button type="submit">submit</button>
+          </div>
+          <div style={{ margin: "0 4px" }}>
+            <button>cancel</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
