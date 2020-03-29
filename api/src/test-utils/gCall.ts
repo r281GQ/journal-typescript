03a860e8fc99eb92eb095f07ad37d79832d75c9e
Endre Vegh <endre.vegh@yahoo.com>
2020-03-29 11:29:00 +0200

    move test utils

---
+++ api/src/test-utils/gCall.ts
@@ -0,0 +1,49 @@
+import { graphql, GraphQLSchema, ExecutionResult } from "graphql";
+import { buildSchema } from "type-graphql";
+
+import { CreateUser } from "../resolvers/CreateUserResolver";
+import { LoginResolver } from "../resolvers/LoginResolver";
+import { CreateEntry } from "../resolvers/CreateEntryResolver";
+
+interface Options<Y = {}> {
+  source: string;
+  variableValues?: Y;
+  // variableValues?: Maybe<{
+  //   [key: string]: any;
+  // }>;
+  contextValue?: any;
+}
+
+let schema: GraphQLSchema;
+
+async function getSchema() {
+  schema = await buildSchema({
+    resolvers: [CreateUser, LoginResolver, CreateEntry]
+  });
+}
+
+export async function executeGraphQL<T = {}, Y = {}>({
+  source,
+  variableValues,
+  contextValue
+}: Options<Y>): Promise<ExecutionResult<T>> {
+  await getSchema();
+
+  let mockedContext = {
+    res: {
+      cookie: jest.fn()
+    },
+    req: {}
+  };
+
+  if (contextValue) {
+    mockedContext = contextValue;
+  }
+
+  return graphql<T>({
+    schema,
+    source,
+    variableValues,
+    contextValue: mockedContext
+  });
+}
